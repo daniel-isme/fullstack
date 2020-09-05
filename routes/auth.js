ccd36bb473e59b5a5888f6602faa811cee2e35cd
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-09-05 19:00:37 +0700

    Добавил роут для авторизации

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const controller = require('../controllers/auth')
 const router = express.Router()
 
+// localhost:5000/api/auth/login
+router.get('/login', controller.login)
 
-router.get('/login', (req, res) => {
-    res.status(200).json({
-        login: true
-    })
-})
+// localhost:5000/api/auth/register
+router.get('/register', controller.register)
+
 
 
 module.exports = router
