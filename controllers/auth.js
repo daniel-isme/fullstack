fe2d405c4c7943a016854d0499cb2099dd9bcc8c
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-09-15 23:24:28 +0700

    Добавил универсальный обработчик ошибки

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
 
 
 module.exports.login = async function (req, res) {
@@ -58,7 +59,7 @@ module.exports.register = async function (req, res) {
             await user.save()
             res.status(201).json(user)
         } catch (e) {
-            //TODO: Обработать ошибку
+            errorHandler(res, e)
         }
     }
 }
