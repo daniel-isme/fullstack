86884c53d3cbf3c0040380d369c2043e11ac3312
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-09-15 12:31:16 +0700

    Апи для логина

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://daniil:lfybbk1999@cluster0.4bcka.mongodb.net/MEANDB?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://daniil:lfybbk1999@cluster0.4bcka.mongodb.net/MEANDB?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
